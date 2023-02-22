b9f5ad42575f653ad122e420a5f64e21dbe83afa
Shaheen Hadadzadeh <shaheenhad@gmail.com>
2023-02-22 14:50:17 -0500

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,10 @@ import "./Header.css";
 export const Header = () => {
   return (
     <div className="header">
-      <h1 className="header__title">Hey, I'm shaheen</h1>
+      <h1 className="header__title">Hey, I'm shaheen.</h1>
+      <p className="header__desc">
+        Fullstack developer, engineer, lifelong learner
+      </p>
     </div>
   );
 };
