1de4a0b821f2bac15f652993ce09edc8ef93afc9
shaheenhad <shaheenhad@gmail.com>
2023-03-31 18:29:38 -0400

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,47 @@
 import "./Header.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export const Header = () => {
+  useEffect(() => {
+    Aos.init({});
+  }, []);
   return (
     <div className="header">
-      <p className="header__intro">hey, my name is</p>
-      <h1 className="header__title">Shaheen Hadadzadeh.</h1>
-      <p className="header__desc">
-        Fullstack developer, engineer, lifelong learner
+      <p
+        data-aos="fade-up"
+        data-aos-delay="1200"
+        data-aos-duration="1000"
+        className="header__intro"
+      >
+        hey, my name is
+      </p>
+      <h1
+        data-aos="fade-up"
+        data-aos-delay="1250"
+        data-aos-duration="1000"
+        className="header__title"
+      >
+        Shaheen Hadadzadeh.
+      </h1>
+      <p
+        data-aos="fade-up"
+        data-aos-delay="1300"
+        data-aos-duration="1000"
+        className="header__desc"
+      >
+        Fullstack developer, engineer, and lifelong learner
+      </p>
+      <p
+        data-aos="fade-up"
+        data-aos-delay="1350"
+        data-aos-duration="1000"
+        className="header__desc_alt"
+      >
+        I'm a pharmaceutical production engineer, turned software engineer, with
+        an interest in building exceptional web experiences. Keep going to see
+        what I've done so far!
       </p>
     </div>
   );
