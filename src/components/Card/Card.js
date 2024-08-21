dd35c4d737d03e49f7376ca8993baef77f22d666
Shaheen Hadadzadeh <shaheenhad@gmail.com>
2024-08-21 17:36:21 -0400

    feat: make card clickable

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,16 +4,18 @@ import { Icon } from "../Icon/Icon";
 export const Card = ({ name, url, githubURL, image, description }) => {
   return (
     <article className="card">
-      <div className="card__wrapper">
-        <a href={githubURL} target="_blank" rel="noreferrer">
-          <Icon name="GitHub" className="card__icon" />
-        </a>
-        <img className="card__image" alt="project example" src={image}></img>
-      </div>
-      <div className="card__info">
-        <h2 className="card__title">{name}</h2>
-        <p className="card__description">{description}</p>
-      </div>
+      <a className="card__link" href={url} target="_blank" rel="noreferrer">
+        <div className="card__wrapper">
+          <a href={githubURL} target="_blank" rel="noreferrer">
+            <Icon name="GitHub" className="card__icon" />
+          </a>
+          <img className="card__image" alt="project example" src={image}></img>
+        </div>
+        <div className="card__info">
+          <h2 className="card__title">{name}</h2>
+          <p className="card__description">{description}</p>
+        </div>
+      </a>
     </article>
   );
 };
